83ac9ca75dfe33eadb7424804870b1b072c7ee0e
Jason Brazeal <jsonbrazeal@gmail.com>
2017-10-01 01:32:51 -0700

    fixed path error in script

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     license='',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python :: 3.6',
     ],
     keywords='notes',
